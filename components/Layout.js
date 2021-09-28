b27abfbb20f97ccda9ed023b364d2d29bf5b43b8
sasirat <sasirat019@gmail.com>
2021-09-28 14:20:29 +0700

    Add many components

---
+++ components/Layout.js
@@ -0,0 +1,9 @@
+const Layout = (props) => {
+  return (
+    <div className="max-w-2xl mx-auto mt-16">
+      {props.children}
+    </div>
+  )
+}
+
+export default Layout
---
+++ components/MatchBox.js
@@ -0,0 +1,15 @@
+const MatchBox = () => {
+  return (
+    <div className="shadow-md flex p-4 rounded-md">
+      <div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>finished</div>
+      <div className="flex flex-1 justify-center items-center">
+        <div className="">
+          <p>test : test</p>
+          <p>test</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MatchBox
