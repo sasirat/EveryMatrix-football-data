c7226d18ca5a6175e5570e2fdf29d08a860881f8
sasirat <sasirat019@gmail.com>
2021-09-29 22:03:29 +0700

    Add axios in matchbox

--- components/MatchBox.js
+++ components/MatchBox.js
@@ -1,14 +1,44 @@
+import { useState, useEffect } from 'react'
+import axios from '../utils/api';
+
 const MatchBox = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    axios.get('https://api.football-data.org/v2/matches').then((response) => {
+      setData(response.data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <p>Loading</p>;
+  }
+
+  if (data === undefined) {
+    return <p>Data is undefined</p>;
+  }
+
   return (
-    <div className="shadow-md flex p-4 rounded-md">
-      <div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>finished</div>
-      <div className="flex flex-1 justify-center items-center">
-        <div className="">
-          <p>test : test</p>
-          <p>test</p>
+    data.matches.map((items) => (
+      <div className="shadow-md flex px-4 py-6 rounded-md my-4">
+        <div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>{items.utcDate}</div>
+        <div className="flex flex-1 justify-center items-center">
+          <div className="">
+            <p>{items.score.fulltime}</p>
+            <div>
+              <span>{items.homeTeam.name}</span>
+              <span className="mx-4">VS</span>
+              <span>{items.awayTeam.name}</span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    ))
+
   )
 }
 
