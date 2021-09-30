50db5ac4d4da07278558d39c0336c7503aa633db
sasirat <sasirat019@gmail.com>
2021-09-30 17:53:03 +0700

    Update home page

--- components/MatchBox.js
+++ components/MatchBox.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Link from 'next/link';
 import axios from '../utils/api';
+import dayjs from 'dayjs';
 
 const MatchBox = () => {
   const [data, setData] = useState();
@@ -27,15 +28,35 @@ const MatchBox = () => {
     data.matches.map((item) => (
       <Link href={`/matches/${item.id}`}>
         <div className="shadow-md flex px-4 py-6 rounded-md my-4">
-          <div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>{item.utcDate}</div>
+          {item.status == "FINISHED" ?
+            (<div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>
+              finished
+            </div>) :
+            (<div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>
+              {dayjs(item.utcDate).format('HH:mm')}
+            </div>)}
           <div className="flex flex-1 justify-center items-center">
-            <div className="">
-              <p>{item.score.fulltime}</p>
-              <div>
-                <span>{item.homeTeam.name}</span>
-                <span className="mx-4">VS</span>
-                <span>{item.awayTeam.name}</span>
-              </div>
+            <div>
+              {item.status == "FINISHED" ?
+                (<> <p className="text-center font-bold text-lg">
+                  <span>{item.score.fullTime.homeTeam}</span>
+                  <span className="mx-4">:</span>
+                  <span>{item.score.fullTime.awayTeam}</span>
+                </p>
+                  <div>
+                    <span>{item.homeTeam.name}</span>
+                    <span className="mx-4">VS</span>
+                    <span>{item.awayTeam.name}</span>
+                  </div>
+                </>
+                ) : (
+                  <div>
+                    <span>{item.homeTeam.name}</span>
+                    <span className="mx-4">VS</span>
+                    <span>{item.awayTeam.name}</span>
+                  </div>
+                )
+              }
             </div>
           </div>
         </div>
