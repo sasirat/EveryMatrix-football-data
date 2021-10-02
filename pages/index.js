d7ec66d23afbae0d2b3f199b2d62f4fe6a5ec11e
sasirat <sasirat019@gmail.com>
2021-10-02 10:21:44 +0700

    Add today in home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import MatchBox from "../components/MatchBox";
 import Layout from '../components/Layout'
+import dayjs from 'dayjs'
 
 const Home = () => {
   return (
     <div>
       <Layout>
         <div className="mb-4">
-          <p className="text-3xl font-bold mb-8">Matchs</p>
+          <div className="flex">
+            <p className="text-3xl font-bold mb-8">Matchs</p>
+            <div className="flex-1 flex" style={{ justifyContent: 'flex-end', color: '#439287' }}>
+              <p className="text-2xl font-bold">{dayjs().format('DD/MM/YYYY')}</p>
+            </div>
+          </div>
           <p className="text-2xl font-bold">Today</p>
         </div>
         <MatchBox />
