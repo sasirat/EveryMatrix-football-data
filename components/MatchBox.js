8dba7b62cb23ff204bf854e3b445ff845b804ec6
sasirat <sasirat019@gmail.com>
2021-10-04 16:34:19 +0700

    Add no match condition

--- components/MatchBox.js
+++ components/MatchBox.js
@@ -24,44 +24,55 @@ const MatchBox = () => {
   }
 
   return (
-    data.matches.map((item) => (
-      <Link href={`/matches/${item.id}`} key={item.id}>
-        <div className="shadow-md flex px-4 py-6 rounded-md my-4 md:text-base text-xs">
-          {item.status == "FINISHED" ?
-            (<div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>
-              finished
-            </div>) :
-            (<div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>
-              {dayjs(item.utcDate).format('HH:mm')}
-            </div>)}
-          <div className="flex flex-1 justify-center items-center ml-4 text-center">
-            <div>
+    <>
+      {data.count != 0 ? (
+        data.matches.map((item) => (
+          <Link href={`/matches/${item.id}`} key={item.id}>
+            <div className="shadow-md flex px-4 py-6 rounded-md my-4 md:text-base text-xs">
               {item.status == "FINISHED" ?
-                (<div>
-                  <div className="text-center font-bold text-lg flex">
-                    <p>{item.score.fullTime.homeTeam}</p>
-                    <p className="mx-4">:</p>
-                    <p>{item.score.fullTime.awayTeam}</p>
-                  </div>
-                  <div className="flex">
-                    <p>{item.homeTeam.name}</p>
-                    <p className="mx-4">VS</p>
-                    <p>{item.awayTeam.name}</p>
-                  </div>
+                (<div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>
+                  finished
+                </div>) :
+                (<div className="py-1 px-4 rounded-full font-bold m-auto" style={{ backgroundColor: '#D6EFEB', color: '#439287' }}>
+                  {dayjs(item.utcDate).format('HH:mm')}
+                </div>)}
+              <div className="flex flex-1 justify-center items-center ml-4 text-center">
+                <div>
+                  {item.status == "FINISHED" ?
+                    (<div>
+                      <div className="text-center font-bold text-lg flex">
+                        <p>{item.score.fullTime.homeTeam}</p>
+                        <p className="mx-4">:</p>
+                        <p>{item.score.fullTime.awayTeam}</p>
+                      </div>
+                      <div className="flex">
+                        <p>{item.homeTeam.name}</p>
+                        <p className="mx-4">VS</p>
+                        <p>{item.awayTeam.name}</p>
+                      </div>
+                    </div>
+                    ) : (
+                      <div className="flex">
+                        <p>{item.homeTeam.name}</p>
+                        <p className="mx-4">VS</p>
+                        <p>{item.awayTeam.name}</p>
+                      </div>
+                    )
+                  }
                 </div>
-                ) : (
-                  <div className="flex">
-                    <p>{item.homeTeam.name}</p>
-                    <p className="mx-4">VS</p>
-                    <p>{item.awayTeam.name}</p>
-                  </div>
-                )
-              }
+              </div>
             </div>
-          </div>
+          </Link>
+        )
+        )
+      ) : (
+        <div className="text-center text-gray-300 mt-4">
+          No matches today
         </div>
-      </Link>
-    ))
+      )
+      }
+
+    </>
   )
 
 }
